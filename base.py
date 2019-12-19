30bc28fc72cbb0dfe00fd810dc17bd7fde038eee
PhoeniXWhite <957360688@qq.com>
2019-12-19 21:39:21 +0800

    nothing

--- base.py
+++ base.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 # MongoDB的客户端
-client = MongoClient("mongodb://admin:123456@localhost:27017,localhost:28018,localhost:29019")
+client = MongoClient("mongodb://root:123456@localhost:27017,localhost:28018,localhost:29019")
 # MongoDB database
 db = client['dobby_db']
 # collection 相当于 table
